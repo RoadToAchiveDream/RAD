607a0c3c36382e7836b1f4873699d3995859d2ec
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 19:01:43 +0500

    Changed

--- RAD_BackEnd.DTOs/Notes/NoteUpdateModel.cs
+++ RAD_BackEnd.DTOs/Notes/NoteUpdateModel.cs
@@ -1,10 +1,9 @@
 ﻿using RAD_BackEnd.Domain.Enums.NoteEnums;
 namespace RAD_BackEnd.DTOs.Notes;
-public record NoteUpdateModel
-{
-    public long Id { get; set; }
-    public long UserId { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
-    public Category category { get; set; }
-}
+public record NoteUpdateModel(
+    long Id,
+    long UserId,
+    string Title,
+    string Content,
+    Category category
+    );
--- RAD_BackEnd.DTOs/Notes/NoteViewModel.cs
+++ RAD_BackEnd.DTOs/Notes/NoteViewModel.cs
@@ -1,10 +1,10 @@
 ﻿using RAD_BackEnd.Domain.Enums.NoteEnums;
 namespace RAD_BackEnd.DTOs.Notes;
-public record NoteViewModel
-{
-    public long Id { get; set; }
-    public long UserId { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
-    public Category category { get; set; }
-}
+public record NoteViewModel(
+    long Id,
+    long UserId,
+    string Title,
+    string Content,
+    Category category
+    );
+
