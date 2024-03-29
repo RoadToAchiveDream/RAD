211b48dc180a51c35cdf5ea0508a8222d262459f
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 13:39:05 +0500

    Removed Plans, PlansTasks, Tasks, User

--- RAD_BackEnd.Domain/Entities/Tasks.cs
+++
@@ -1,9 +0,0 @@
-﻿using RAD_BackEnd.Domain.Commons;
-
-namespace RAD_BackEnd.Domain.Entities;
-
-public class Tasks : Auditable
-{
-    public long UserId { get; set; }
-    public string Body { get; set; }
-}
