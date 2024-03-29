3f62fed4d7ae8285ee0ee640e7daef3db1c9b885
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 16:36:52 +0500

    Created DTO's :User

---
+++ RAD_BackEnd.DTOs/Users/UserUpdateModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RAD_BackEnd.DTOs.Users;
+public record UserUpdateModel
+{
+    public long Id {  get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string ProfilePicture { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
