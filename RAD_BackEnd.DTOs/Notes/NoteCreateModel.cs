b6ab570e5dc356dd9cbf2f04e328c05f0eae397f
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 16:17:30 +0500

    Created DTO'S:Note

---
+++ RAD_BackEnd.DTOs/Notes/NoteCreateModel.cs
@@ -0,0 +1,10 @@
+﻿using RAD_BackEnd.Domain.Enums.NoteEnums;
+
+namespace RAD_BackEnd.DTOs.Notes;
+public record NoteCreateModel
+{
+    public long UserId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public Category category { get; set; }
+}
