89c4cca429864a2a337a08c79432f52388bdb2de
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 13:52:31 +0500

    Created Note Entity

---
+++ RAD_BackEnd.Domain/Entities/Note.cs
@@ -0,0 +1,11 @@
+﻿using RAD_BackEnd.Domain.Commons;
+namespace RAD_BackEnd.Domain.Entities;
+public class Note : Auditable
+{
+    public long Id { get; set; }
+    public long UserId { get; set; }
+    public User user { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public Category category { get; set; }
+}
