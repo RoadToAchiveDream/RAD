047aa850c51ebc37148942ecc57706c71482ae2f
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 13:38:03 +0500

    Removed Dream

--- RAD_BackEnd.Domain/Entities/Dream.cs
+++
@@ -1,9 +0,0 @@
-﻿using RAD_BackEnd.Domain.Commons;
-
-namespace RAD_BackEnd.Domain.Entities;
-
-public class Dream : Auditable
-{
-    public long UserId { get; set; }
-    public string Body { get; set; }
-}
