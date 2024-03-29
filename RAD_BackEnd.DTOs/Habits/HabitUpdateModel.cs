dc982d6b2fef171f3a3e56e274eb2168b8faab8e
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 19:39:25 +0500

    Update HabitUpdateModel.cs

--- RAD_BackEnd.DTOs/Habits/HabitUpdateModel.cs
+++ RAD_BackEnd.DTOs/Habits/HabitUpdateModel.cs
@@ -1,8 +1,8 @@
 ﻿using RAD_BackEnd.Domain.Enums.HabitEnums;
 namespace RAD_BackEnd.DTOs.Habits;
 public record HabitUpdateModel(
-    long Id,
-    long UserId,
+     long Id,
+     long UserId,
      string Name,
      string Description,
      DateTime StartDate,
