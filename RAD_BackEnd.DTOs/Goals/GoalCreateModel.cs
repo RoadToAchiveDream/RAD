eb48ebbca0db90914183bdea0acdb9098017df8d
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 19:21:50 +0500

    Chnaged class to record

--- RAD_BackEnd.DTOs/Goals/GoalCreateModel.cs
+++ RAD_BackEnd.DTOs/Goals/GoalCreateModel.cs
@@ -1,12 +1,11 @@
 ﻿using RAD_BackEnd.Domain.Enums.GoalEnums;
 namespace RAD_BackEnd.DTOs.Goals;
-public record GoalCreateModel
-{
-    public long UserId { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
-    public Status status { get; set; }
-    public decimal Progress { get; set; }
-}
+public record GoalCreateModel(
+    long UserId,
+    string Title,
+    string Description ,
+    DateTime StartTime,
+    DateTime EndTime, 
+    Status status ,
+    decimal Progress);
+
