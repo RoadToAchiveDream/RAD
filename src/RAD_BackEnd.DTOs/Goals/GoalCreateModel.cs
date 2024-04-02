cc62c91fdb1d43500fa95186178ff422250ac559
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-02 09:30:20 +0500

    edited goalcreatemodel

--- src/RAD_BackEnd.DTOs/Goals/GoalCreateModel.cs
+++ src/RAD_BackEnd.DTOs/Goals/GoalCreateModel.cs
@@ -1,11 +1,14 @@
 ﻿using RAD_BackEnd.Domain.Enums.GoalEnums;
+
 namespace RAD_BackEnd.DTOs.Goals;
-public record GoalCreateModel(
-    long UserId,
-    string Title,
-    string Description ,
-    DateTime StartTime,
-    DateTime EndTime, 
-    Status Status ,
-    decimal Progress);
+public class GoalCreateModel
+{
+    public long UserId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public Status Status { get; set; }
+    public decimal Progress { get; set; }
+}
 
