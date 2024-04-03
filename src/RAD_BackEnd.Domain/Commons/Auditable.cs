4a109f53a487870653586b470d52c0066140485c
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-03 14:17:38 +0500

    edited auditable

--- src/RAD_BackEnd.Domain/Commons/Auditable.cs
+++ src/RAD_BackEnd.Domain/Commons/Auditable.cs
@@ -2,10 +2,8 @@
 public class Auditable
 {
     public long Id { get; set; }
-    public DateTimeOffset Created_At { get; set; }
-    public DateTimeOffset Updated_At { get; set; }
-    public DateTimeOffset Deleted_At { get; set; }
-    public bool Is_Deleted { get; set; } = false;
-    public bool Is_Completed { get; set; } = false;
-    public bool Is_Achieved { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+    public bool IsDeleted { get; set; }
 }
