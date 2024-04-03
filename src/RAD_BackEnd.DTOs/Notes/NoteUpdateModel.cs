5049024e34a845b3b75eb3221def963b47ab7869
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-03 11:59:40 +0500

    removed Id property from all DTOs

--- src/RAD_BackEnd.DTOs/Notes/NoteUpdateModel.cs
+++ src/RAD_BackEnd.DTOs/Notes/NoteUpdateModel.cs
@@ -2,7 +2,6 @@
 namespace RAD_BackEnd.DTOs.Notes;
 public record NoteUpdateModel
 {
-    public long Id { get; set; }
     public long UserId { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
