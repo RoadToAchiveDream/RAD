50dd8dc67a58c9a3ae1465cf344c48b00c54962f
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-23 02:24:25 +0500

    fixed namespaces

--- src/RAD_BackEnd.DataAccess/Contexts/AppDbContext.cs
+++ src/RAD_BackEnd.DataAccess/Contexts/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RAD_BackEnd.Domain.Entities;
+using Task = RAD_BackEnd.Domain.Entities.Task;
 
 namespace RAD_BackEnd.DataAccess.Contexts;
 public class AppDbContext : DbContext
@@ -8,7 +9,7 @@ public class AppDbContext : DbContext
 
     #region DbSet Properties
     public DbSet<User> Users { get; set; }
-    public DbSet<Domain.Entities.Task> Tasks { get; set; }
+    public DbSet<Task> Tasks { get; set; }
     public DbSet<Event> Events { get; set; }
     public DbSet<Habit> Habits { get; set; }
     public DbSet<Note> Notes { get; set; }
