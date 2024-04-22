2d2629fb827f9ed82503fe9060d58194b969bd94
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-23 00:25:42 +0500

    Wrote INoteService

--- src/RAD_BackEnd.Services/Services/Notes/INoteService.cs
+++ src/RAD_BackEnd.Services/Services/Notes/INoteService.cs
@@ -1,6 +1,12 @@
-﻿namespace RAD_BackEnd.Services.Services.Notes
+﻿using RAD_BackEnd.DTOs.Notes;
+
+namespace RAD_BackEnd.Services.Services.Notes;
+
+public interface INoteService
 {
-    internal class INoteService
-    {
-    }
+    ValueTask<NoteViewModel> CreateAsync(NoteCreateModel note);
+    ValueTask<NoteViewModel> UpdateAsync(long id, NoteUpdateModel note);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<NoteViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<NoteViewModel>> GetAllAsync();
 }
