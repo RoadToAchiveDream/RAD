bf309cf375339a5fe96ead347a42a075cf38f901
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-23 00:27:40 +0500

    Wrote ITaskService

--- src/RAD_BackEnd.Services/Services/Tasks/ITaskService.cs
+++ src/RAD_BackEnd.Services/Services/Tasks/ITaskService.cs
@@ -1,6 +1,12 @@
-﻿namespace RAD_BackEnd.Services.Services.Tasks
+﻿using RAD_BackEnd.DTOs.Tasks;
+
+namespace RAD_BackEnd.Services.Services.Tasks;
+
+public interface ITaskService
 {
-    internal class ITaskService
-    {
-    }
+    ValueTask<TaskViewModel> CreateAsync(TaskCreateModel task);
+    ValueTask<TaskViewModel> UpdateAsync(long id, TaskUpdateModel task);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<TaskViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<TaskViewModel>> GetAllAsync();
 }
