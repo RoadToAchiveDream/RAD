0389811e94897eecd7826f2c0a24ad8746e8c651
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-23 00:29:47 +0500

    Wrote IUserService

--- src/RAD_BackEnd.Services/Services/Users/IUserService.cs
+++ src/RAD_BackEnd.Services/Services/Users/IUserService.cs
@@ -1,6 +1,12 @@
-﻿namespace RAD_BackEnd.Services.Services.Users
+﻿using RAD_BackEnd.DTOs.Users;
+
+namespace RAD_BackEnd.Services.Services.Users;
+
+public interface IUserService
 {
-    internal class IUserService
-    {
-    }
+    ValueTask<UserViewModel> CreateAsync(UserCreateModel user);
+    ValueTask<UserViewModel> UpdateAsync(long id, UserUpdateModel user);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<UserViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<UserViewModel>> GetAllAsync();
 }
