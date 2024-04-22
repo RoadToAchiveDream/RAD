ff87c4215540ae15335381b60790f7684971ad40
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-23 00:17:04 +0500

    Wrote IEventService

--- src/RAD_BackEnd.Services/Services/Events/IEventService.cs
+++ src/RAD_BackEnd.Services/Services/Events/IEventService.cs
@@ -1,6 +1,12 @@
-﻿namespace RAD_BackEnd.Services.Services.Events
+﻿using RAD_BackEnd.DTOs.Events;
+
+namespace RAD_BackEnd.Services.Services.Events;
+
+public interface IEventService
 {
-    internal class IEventService
-    {
-    }
+    ValueTask<EventViewModel> CreateAsync(EventCreateModel @event);
+    ValueTask<EventViewModel> UpdateAsync(long id, EventUpdateModel @event);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<EventViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<EventViewModel>> GetAllAsync();
 }
