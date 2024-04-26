b4af0b6b016cb59fe1912a5243bacdf76d2126bb
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-26 19:33:32 +0500

    not completed

--- src/RAD_BackEnd.DTOs/Users/UserCreateModel.cs
+++ src/RAD_BackEnd.DTOs/Users/UserCreateModel.cs
@@ -1,7 +1,6 @@
 ﻿namespace RAD_BackEnd.DTOs.Users;
 public class UserCreateModel
 {
-    public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
     public string ProfilePicture { get; set; }
---
+++ src/RAD_BackEnd.Services/Services/Users/UserService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using RAD_BackEnd.DataAccess.UnintOfWorks;
+using RAD_BackEnd.DTOs.Users;
+
+namespace RAD_BackEnd.Services.Services.Users;
+
+public class UserService(IUnitOfWork unitOfWork, IMapper mapper) : IUserService
+{
+    public async ValueTask<UserViewModel> CreateAsync(UserCreateModel user)
+    {
+        var existsUser = await unitOfWork.Users.SelectAsync(
+            expression: u => u.Email == user.Email && !u.IsDeleted);
+
+        if (existsUser is not null)
+            throw new Exception();
+    }
+
+    public ValueTask<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<UserViewModel>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<UserViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<UserViewModel> UpdateAsync(long id, UserUpdateModel user)
+    {
+        throw new NotImplementedException();
+    }
+}
