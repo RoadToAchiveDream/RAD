0a6442b1c5bc237735a7a9d1e83b367ea3217e8a
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-26 19:40:10 +0500

    Wrote ArgumentNotValidException

---
+++ src/RAD_BackEnd.Services/Exceptions/ArgumentNotValidException.cs
@@ -0,0 +1,10 @@
+﻿namespace RAD_BackEnd.Services.Exceptions;
+
+public class ArgumentNotValidException : Exception
+{
+    public ArgumentNotValidException() { }
+    public ArgumentNotValidException(string message) : base(message) { }
+    public ArgumentNotValidException(string message, Exception exception) { }
+
+    public int StatusCode => 400;
+}
