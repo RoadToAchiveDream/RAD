c2cc114fe8ecc02f4e39f3c72bf1befe14d04767
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-27 23:13:49 +0500

    Wrote UserController

---
+++ src/RAD_BackEnd.WebApi/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RAD_BackEnd.DTOs.Users;
+using RAD_BackEnd.Services.Services.Users;
+using RAD_BackEnd.WebApi.Models;
+
+namespace RAD_BackEnd.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IUserService userService) : ControllerBase
+{
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAllAsync()
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await userService.GetAllAsync()
+        });
+    }
+    [HttpGet("{id:long}")]
+    public async ValueTask<IActionResult> GetByIdAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await userService.GetByIdAsync(id)
+        });
+    }
+    [HttpPost]
+    public async ValueTask<IActionResult> PostAsync([FromBody] UserCreateModel user)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await userService.CreateAsync(user)
+        });
+    }
+    [HttpDelete("{id:long}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await userService.DeleteAsync(id)
+        });
+    }
+    [HttpPut("{id:long}")]
+    public async ValueTask<IActionResult> PutAsync(long id, [FromBody] UserUpdateModel user)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await userService.UpdateAsync(id, user)
+        });
+    }
+}
