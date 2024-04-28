91d40fd7a2c0fe0cf4fdff4ff59000ffabd35f6e
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-29 01:24:50 +0500

    Added helpers and extensions

--- src/RAD_BackEnd.WebApi/Program.cs
+++ src/RAD_BackEnd.WebApi/Program.cs
@@ -1,38 +1,43 @@
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
 using RAD_BackEnd.DataAccess.Contexts;
-using RAD_BackEnd.DataAccess.Repositories;
-using RAD_BackEnd.DataAccess.UnintOfWorks;
 using RAD_BackEnd.Services.NewFolder;
-using RAD_BackEnd.Services.Services.Events;
-using RAD_BackEnd.Services.Services.Goals;
-using RAD_BackEnd.Services.Services.Habits;
-using RAD_BackEnd.Services.Services.Notes;
-using RAD_BackEnd.Services.Services.Tasks;
-using RAD_BackEnd.Services.Services.Users;
+using RAD_BackEnd.WebApi.Extensions;
+using RAD_BackEnd.WebApi.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
+builder.Services.AddControllers(options =>
+    options.Conventions.Add(new RouteTokenTransformerConvention(new RouteHelper())));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGenJwt();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ITaskService, TaskService>();
-builder.Services.AddScoped<INoteService, NoteService>();
-builder.Services.AddScoped<IHabitService, HabitService>();
-builder.Services.AddScoped<IGoalService, GoalService>();
-builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddAuthorization();
+builder.Services.AddJWtService(builder.Configuration);
+
+builder.Services.AddServices();
+builder.Services.AddValidators();
+builder.Services.AddMemoryCache();
+
+EnvironmentHelper.WebRootPath = builder.Environment.WebRootPath;
 
 var app = builder.Build();
+app.InjectEnvironmentItems();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<AppDbContext>();
+
+    dbContext.Database.EnsureCreated();
+    dbContext.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -42,8 +47,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.Run();
